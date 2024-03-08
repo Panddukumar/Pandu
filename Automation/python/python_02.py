af9ec5cad18936a9125b3f0d87f1495b97d23615
Pandukumar <155426908+Panddukumar@users.noreply.github.com>
2024-03-08 09:46:55 +0530

    Delete Automation/python directory

--- Automation/python/python_02.py
+++
@@ -1,6 +0,0 @@
-# union method to update
-
-set1 = {"a", "b" , "c"}
-set2 = {1, 2, 3}
-set3=set1.union(set2)
-print(set3)
--- Automation/python/python_04set.py
+++
@@ -1,3 +0,0 @@
-thisset = {"apple", "banana", "cherry"}
-for i in thisset:
-    print(i)
