9fb84f73cfa8dddbdc687def43978e09dce94892
lenovo <pandukumarbr@gmail.com>
2024-06-06 21:32:06 +0530

    new changes

--- selenium/Best_window_handle.py
+++ selenium/Best_window_handle.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome()
 driver.get("https://docs.python.org/3/tutorial/")
 
 # Wait for the list of links to load
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 10,ignored_exceptions=Exception)
 all_links = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//li[@class='toctree-l1']//a")))
 
 print(len(all_links))
@@ -15,5 +15,4 @@ print(len(all_links))
 for link in all_links:
     url = link.get_attribute("href")
     driver.execute_script("window.open('"+url+"','_blank');")
-    driver.switch_to.window(driver.window_handles[-1])
-    print(driver.current_url)
+    print(driver.title)
---
+++ selenium/newpractise.py
@@ -0,0 +1,3 @@
+list1=[1,2,3,4]
+for i in range(len(list1)):
+    print(list1[-1])
